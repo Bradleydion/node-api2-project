d2d8369cecb2735f1b2f4bd570dd1a49c45788c6
Bradley Dion <bradleydion@MacBook-Pro.local>
2021-02-14 21:35:56 -0500

    stuck on 4 now

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -18,7 +18,7 @@ router.get("/posts", (req,res)=>{
     })
 })
 
-router.get("/posts/:id",(res, req)=>{
+router.get("/posts/:id",(req, res)=>{
     posts.findById(req.params.id)
     .then ((posts)=>{
         if (posts){
@@ -31,7 +31,36 @@ router.get("/posts/:id",(res, req)=>{
         res.status(500).json({message:"The posts information could not be retrieved"})
     })
 })
+router.post("/posts", (req, res)=>{
+    if (!req.body.title || !req.body.contents){
+        res.status(400).json({message:"Please provide title and contents for the post"})}
+        else{ posts.insert(req.body)
+            .then((posts)=>{
+                res.status(201).json(posts)
+            }).catch((err)=>{
+                console.log(err)
+                res.status(500).json({message:"There was an error while saving the post to the database."})
+            })
+
+        }
     
+})
+router.put("/posts/:id", (req, res)=>{
+    if(!req.body.title || req.body.contents){
+        return res.status(400).json({message:"Please provide title and contents for the post"})
+    }
+    posts.update(req.params.id, req.body)
+        .then((posts)=>{
+            if (posts){
+                res.status(200).json(posts)
+            }else{
+                res.status(404).json({message:"The post with the specified id does not exist"})
+            }
+        }).catch((err)=>{
+            console.log(err)
+            res.status(500).json({message:"The posts information could not be modified"})
+        })
+})
 
 
 module.exports=router 
